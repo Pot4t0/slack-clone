68b259d0de6986450286d381ba9d7bb6cc613060
Pot4t0 <tranhoang.minh@yahoo.com>
2018-11-09 14:42:01 +0800

    Change password

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
+const sequelize = new Sequelize('slack', 'postgres', 'password', {
   dialect: 'postgres',
   define: {
     underscored: true,
